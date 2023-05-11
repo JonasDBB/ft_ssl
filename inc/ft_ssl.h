b27eb5c7c3ceaa055f82bc91e80f3f13023ad1d1
jbennink <jbennink@student.codam.nl>
2023-05-11 16:07:39 +0200

    initial commit

---
+++ inc/ft_ssl.h
@@ -0,0 +1,8 @@
+//
+// Created by Jonas Bennink bolt on 5/11/23.
+//
+
+#ifndef FT_SSL_FT_SSL_H
+#define FT_SSL_FT_SSL_H
+
+#endif //FT_SSL_FT_SSL_H
---
+++ src/ft_ssl.c
@@ -0,0 +1,12 @@
+//
+// Created by Jonas Bennink bolt on 5/11/23.
+//
+
+#include "ft_ssl.h"
+#include "ft_clib.h"
+#include <stdio.h>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
